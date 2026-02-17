9b5cd5e9dee38d6e4317ab23a28cb663c30bf51d
sangaruviswanadham0312 <sangaruviswanadham0312@gmail.com>
2026-02-17 15:37:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, jsonify
+import librosa
+import numpy as np
+import joblib
+import os
+
+app = Flask(__name__)
+
+# Models load chesthunnam
+model = joblib.load('final_emotion_model.pkl')
+scaler = joblib.load('final_scaler.pkl')
+
+def extract_features(path):
+    # Powerful features extraction logic
+    data, sample_rate = librosa.load(path, duration=2.5, offset=0.6)
+    mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate, n_mfcc=40).T, axis=0)
+    mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate).T, axis=0)
+    contrast = np.mean(librosa.feature.spectral_contrast(y=data, sr=sample_rate).T, axis=0)
+    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(data), sr=sample_rate).T, axis=0)
+    return np.hstack([mfcc, mel, contrast, tonnetz])
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict_offline', methods=['POST'])
+def predict_offline():
+    file = request.files['file']
+    path = os.path.join('uploads', file.filename)
+    file.save(path)
+    feat = extract_features(path)
+    feat = scaler.transform([feat])
+    prediction = model.predict(feat)[0]
+    return jsonify({'emotion': f"Detected: {prediction}"})
+
+@app.route('/predict_online', methods=['POST'])
+def predict_online():
+    audio_data = request.files['audio']
+    path = os.path.join('uploads', 'live_audio.wav')
+    audio_data.save(path)
+    feat = extract_features(path)
+    feat = scaler.transform([feat])
+    prediction = model.predict(feat)[0]
+    return jsonify({'emotion': f"Real Emotion: {prediction}"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
